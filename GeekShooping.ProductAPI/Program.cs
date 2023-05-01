6801f71d32b385047f32583a47abd3f6d01255af
rodrigo barreto <rodrigo_.barreto@hotmail.com>
2023-05-01 12:41:17 -0300

    Criando PartilLogin, configurando classes para uso de autorização

--- GeekShooping.ProductAPI/Program.cs
+++ GeekShooping.ProductAPI/Program.cs
@@ -3,19 +3,12 @@ using GeekShooping.ProductAPI.Config;
 using GeekShooping.ProductAPI.Model.Context;
 using GeekShooping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
-});
 
 var connection = builder.Configuration["MySQlConnection:MySQlConnectionString"];
 builder.Services.AddDbContext<MySqlContext>(options => options
@@ -28,6 +21,65 @@ builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    }
+  );
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping");
+    });
+});
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer'  [space] abd your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In= ParameterLocation.Header
+                        },
+                        new List<string> ()
+                    }
+                });
+});
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,6 +89,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
--- GeekShooping.Web/Program.cs
+++ GeekShooping.Web/Program.cs
@@ -1,5 +1,6 @@
 using GeekShooping.Web.Services;
 using GeekShooping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 
 namespace GeekShooping.Web
 {
@@ -10,10 +11,34 @@ namespace GeekShooping.Web
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+
             builder.Services.AddHttpClient<IProductService, ProductService>(c =>
                 c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])
                 );
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            }
+            ).AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+             .AddOpenIdConnect("oidc", options =>
+             {
+                 options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
+                 options.GetClaimsFromUserInfoEndpoint = true;
+                 options.ClientId = "geek_shopping";
+                 options.ClientSecret = "my_super_secret";
+                 options.ResponseType = "code";
+                 options.ClaimActions.MapJsonKey("role", "role", "role");
+                 options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+                 options.TokenValidationParameters.NameClaimType = "name";
+                 options.TokenValidationParameters.RoleClaimType = "role";
+                 options.Scope.Add("geek_shopping");
+                 options.SaveTokens = true;
+             });
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -26,6 +51,8 @@ namespace GeekShooping.Web
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
