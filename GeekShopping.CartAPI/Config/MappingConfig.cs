2c3c734625a7d8a8cb4e04d27c661a106ab8d95f
rodrigo barreto <rodrigo_.barreto@hotmail.com>
2023-05-05 12:05:28 -0300

    Criando CartController, ajustando MappingConfig, Criando as classes VO, primeira migration do serviço de carrinho(cart), ajustando a classe de contexto, criando meus repositorios e deletando classe e controle desnecessários (WeatherForecast)

--- GeekShopping.CartAPI/Config/MappingConfig.cs
+++ GeekShopping.CartAPI/Config/MappingConfig.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using GeekShopping.CartAPI.Data.ValueObjects;
 using GeekShopping.CartAPI.Model;
 
 namespace GeekShopping.CartAPI.Config
@@ -9,8 +10,21 @@ namespace GeekShopping.CartAPI.Config
         {
             var mappingConfig = new MapperConfiguration(config =>
             {
+                //1ª Uma maneira de fazer, utilizarei a primeira
+                config.CreateMap<ProductVO, Product>().ReverseMap();
+                config.CreateMap<CartHeaderVO, CartHeader>().ReverseMap();
+                config.CreateMap<CartDetailVO, CartDetail>().ReverseMap();
+                config.CreateMap<CartVO, Cart>().ReverseMap();
+
+                //2ªª outra maneira de fazer
                 //config.CreateMap<ProductVO, Product>();
                 //config.CreateMap<Product, ProductVO>();
+                //config.CreateMap<CartHeaderVO, CartHeader>();
+                //config.CreateMap<CartHeader, CartHeaderVO>();
+                //config.CreateMap<CartDetailVO, CartDetail>();
+                //config.CreateMap<CartDetail, CartDetailVO>();
+                //config.CreateMap<CartVO, Cart>();
+                //config.CreateMap<Cart, CartVO>();
             });
             return mappingConfig;
         }
