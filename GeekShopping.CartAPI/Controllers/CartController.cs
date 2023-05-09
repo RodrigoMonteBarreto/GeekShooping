3f515accf3fbea269d3a5dcaf84912fd65593892
rodrigo barreto <rodrigo_.barreto@hotmail.com>
2023-05-09 11:32:29 -0300

    Integrando tela de checkout ao microsserviço  CartAPI

--- GeekShopping.CartAPI/Controllers/CartController.cs
+++ GeekShopping.CartAPI/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Messages;
 using GeekShopping.CartAPI.Repository;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
@@ -65,6 +66,16 @@ namespace GeekShopping.CartAPI.Controllers
             if (!status) return NotFound();
             return Ok(status);
         }
-    }
 
+        [HttpPost("checkout")]
+        public async Task<ActionResult<CheckoutHeaderVO>> Checkout(CheckoutHeaderVO vo)
+        {
+            var cart = await _repository.FindCartByUserId(vo.UserId);
+            if (cart == null) return NotFound();
+            vo.CartDetails = cart.CartDetails;
+            vo.DateTime = DateTime.Now;
+
+            return Ok(vo);
+        }
+    }
 }
